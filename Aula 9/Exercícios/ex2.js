94892593be1caf33ce7e64f53a3ec57423045c56
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-05-17 11:36:36 -0300

    Create ex2.js

---
+++ Aula 9/Exercícios/ex2.js
@@ -0,0 +1,6 @@
+Nomes = ['R$2.500,00, R$50.000,00, R$4.000,00, R$3.500,00, R$9.000,00, R$6.000,00, R$1.250,00, R$0000,00,']
+
+for (let index = 0; index < Nomes.length; index++) {
+    console.log(Nomes[index]);
+    
+}
