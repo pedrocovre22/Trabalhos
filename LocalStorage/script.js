02d78bce8501cb78a084c42432fcbdd83ce1108b
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-05-24 12:53:20 -0300

    Create script.js

---
+++ LocalStorage/script.js
@@ -0,0 +1,11 @@
+let usuários = []
+
+const addUsuários = () => {
+    let nome = document.getElementById('nome').value
+    let nome = document.getElementById('email').value
+    let nome = document.getElementById('senha').value
+
+    usuários.push([nome, email, senha])
+    console.log('users', usuarios);
+    
+}
