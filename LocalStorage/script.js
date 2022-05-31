bf565240f3dec33c91c867612f82e694233aae44
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-05-31 12:35:41 -0300

    Update script.js

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -5,7 +5,24 @@ const addUsuários = () => {
     let nome = document.getElementById('email').value
     let nome = document.getElementById('senha').value
 
+    if(localStorage.getItem('usuarios') !=null){
+        usuarios = JSON.parse(localStorage.getItem('usuarios'))
+        usuarios.forEach(usuarios => {
+            console.log(usuarios[0], usuarios[1])
+            tbody.innerHTML += "<tr> <td>"+usuario[0]+"</td> <td>"+usuario[1]+"</td> </tr>"
+            
+        });
+    }else {
+        tbody.innerHTML += "<tr> <td>Vazio</td> <td>Vazio</td> </tr>"
+    }
+
     usuários.push([nome, email, senha])
-    console.log('users', usuarios);
-    
+    localStorage.setItem('usuarios', JSON.stringify(usuarios))
+}
+
+const listUsuarios = () => {
+    let tbody = document.getElementById('table-linha')
+
+    tbody.innerHTML += "<tr> <td>Pedro</td> <td>pedro_covre@estudante.sesisenai.org.br<td> </tr>"
+
 }
