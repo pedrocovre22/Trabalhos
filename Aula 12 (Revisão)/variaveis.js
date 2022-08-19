ac8af11e10e3a087426ac50defd25c33e16d2278
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-08-19 10:53:30 -0300

    Create variaveis.js

---
+++ Aula 12 (Revisão)/variaveis.js
@@ -0,0 +1,18 @@
+let numeroInteiro = 10
+console.log(numeroInteiro, typeof(numeroInteiro))
+
+let numeroReal = 10.50
+console.log(numeroReal, typeof(numeroReal))
+
+let nome = 'Mônica'
+console.log(nome, typeof(nome))
+
+let isVazio = nome == "Mônica"
+console.log(isVazio, typeof(isVazio))
+
+let number = numeroInteiro == '1'
+console.log(isNumber, typeof(isNumber))
+
+const PORT = 3000
+const URI = 'HTTP://localhost:$(PORT)'
+console.log(URI)  
