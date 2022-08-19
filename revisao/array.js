4f565474a782229d310f61ec98c4e5801855e7d2
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-08-19 10:54:25 -0300

    Delete revisao directory

--- revisao/array.js
+++
@@ -1,4 +0,0 @@
-let nome = ['Mônica', 'Cebolinha', 'Cascão', 'Magali']
-nomes.array.forEach(nome => {
-    console.log(nome, nome.length)
-})
--- revisao/while.js
+++
@@ -1,7 +0,0 @@
-
-let isImpar = true
-while(isImpar){
-    let numeor = Math.floor(Math.random() * 10000)
-    console.log(numero)
-    isImpar = numero % 2 == 1
-}
