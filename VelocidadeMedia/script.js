6fe6a83728060dcbe8d19310fc1af6deda2436e4
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-09-20 12:53:20 -0300

    Update script.js

--- VelocidadeMedia/script.js
+++ VelocidadeMedia/script.js
@@ -1,13 +1,24 @@
-function  calculate( )
-    {
-        var dist = document.getElementById("deslocamento").value
-        var speed = document.getElementById("tempo").value;
+let velocidades = []
 
-        document.getElementById("velocidadeMedia").value = dist / speed;
+function calculate() {
+
+    if (localStorage.getItem('velocidades') !== null) {
+        velocidades = JSON.parse(localStorage.getItem('velocidades'))
     }
 
-    function limpar () {
-        document.getElementById("deslocamento").value = 0
-        document.getElementById("tempo").value = 0
-        document.getElementById("velocidadeMedia").value= 0
-      }
+    var dist = document.getElementById("deslocamento").value
+    var speed = document.getElementById("tempo").value;
+
+    document.getElementById("velocidadeMedia").value = dist / speed;
+    velocidades.push(document.getElementById("velocidadeMedia").value)
+    document.getElementById('velocidades').value = velocidades
+    localStorage.setItem('velocidades', JSON.stringify(velocidades))
+}
+
+
+function limpar() {
+    document.getElementById("deslocamento").value = 0
+    document.getElementById("tempo").value = 0
+    document.getElementById("velocidadeMedia").value = 0
+}
+
