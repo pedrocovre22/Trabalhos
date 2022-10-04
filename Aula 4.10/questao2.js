f35d096d04be7f1e07ee10519b9fa4bd4af3811f
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-10-04 12:53:19 -0300

    Create questao2.js

---
+++ Aula 4.10/questao2.js
@@ -0,0 +1,8 @@
+var num = parseInt(prompt('digite um numero'))
+
+if (num == 0)
+    console.log('numero igual a zero');
+else if (num > 0)
+    console.log('numero positivo');
+else
+    console.log('numero negativo');
