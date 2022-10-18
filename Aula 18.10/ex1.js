f4ed9672f1f2a499cfca4e4c82bce4c594f16b21
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-10-18 12:21:26 -0300

    Create ex1.js

---
+++ Aula 18.10/ex1.js
@@ -0,0 +1,7 @@
+let num1 = parseFloat(prompt('Digite um numero'))
+        let num2 = parseFloat(prompt('Digite outro numero'))
+        
+        if(num1 > num2)//se 
+          console.log(num1)
+        else //se nao
+          console.log(num2)
