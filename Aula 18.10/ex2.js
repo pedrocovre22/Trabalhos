d13f64b354451eadd79da4a83d747903f45193e9
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-10-18 12:21:46 -0300

    Create ex2.js

---
+++ Aula 18.10/ex2.js
@@ -0,0 +1,10 @@
+let sexo = prompt('digite o seu sexo')
+        
+        if(sexo=='F'){
+          console.log("Voce e uma mulher!")          
+        }
+        if(sexo == 'M'){
+          console.log("Voce e uma mulher!")
+        }else{
+          console.log("Sexo diferente de F ou M")
+        }
