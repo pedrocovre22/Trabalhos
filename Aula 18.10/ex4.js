cf2afd1a753630ac80f012f84a1074f11d90e473
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-10-18 12:22:27 -0300

    Create ex4.js

---
+++ Aula 18.10/ex4.js
@@ -0,0 +1,10 @@
+let idade = parseInt(prompt('Digite sua idade'))
+
+        if(idade <= 10)
+          console.log('Infantil')        
+        else if(idade <= 13)
+          console.log('Infanto Juvenil')        
+        else if(idade <= 18)
+          console.log('Juvenil')        
+        else
+         console.log('Adulto')
