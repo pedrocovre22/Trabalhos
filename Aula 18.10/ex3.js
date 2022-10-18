b03ef136e35aef327421dea6a5358816e86d3e1f
Pedro Covre <100951510+pedrocovre22@users.noreply.github.com>
2022-10-18 12:22:02 -0300

    Create ex3.js

---
+++ Aula 18.10/ex3.js
@@ -0,0 +1,10 @@
+let sexo = prompt('digite o seu sexo')
+        
+        if(sexo=='F'){
+          console.log("Voce e uma mulher!")          
+        }
+        else if(sexo == 'M'){
+          console.log("Voce e uma homem!")
+        }else{
+          console.log("Sexo diferente de F ou M")
+        }
